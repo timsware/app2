b1d223c43c6790923cc20c4d74ab84854b641307
Timothy Milliken <tmillik@hotmail.com>
2018-01-22 13:23:24 -0600

    inital test

---
+++ App2/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace App2
+{
+    [Activity(Label = "App2", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            AppCenter.Start("f500f1ce-749e-41dc-9ee6-84bbd1251295",
+                   typeof(Analytics), typeof(Crashes));
+
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+
+            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+        }
+
+    }
+}
+
